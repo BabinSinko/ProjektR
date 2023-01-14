e7fea673537c9c81b69fbad1f214b9c6ca9f59cf
FilipBonbon <filip.bonbon@gmail.com>
2023-01-14 23:29:34 +0100

    pripremi kod za pokretanje

--- ProjektR/src/main/java/hr/fer/projektr/Main.java
+++ ProjektR/src/main/java/hr/fer/projektr/Main.java
@@ -1,6 +1,9 @@
 package hr.fer.projektr;
+
+import hr.fer.projektr.ai.Training;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello kroneri namazani!");
+        new Training(100).train(10_000, 1000);
     }
 }
