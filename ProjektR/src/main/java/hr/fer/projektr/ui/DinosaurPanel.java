1ce0ed2bad099ca951b6ee52e1889e1443372458
Samuel <samuelsamilucic@gmail.com>
2023-01-19 18:36:19 +0100

    scaling

--- ProjektR/src/main/java/hr/fer/projektr/ui/DinosaurPanel.java
+++ ProjektR/src/main/java/hr/fer/projektr/ui/DinosaurPanel.java
@@ -10,7 +10,7 @@ import hr.fer.projektr.game.GameInterface;
 import hr.fer.projektr.game.entities.Entity;
 
 public class DinosaurPanel extends JPanel implements ActionListener {
-	private final static int DELAY = 1;
+	private final static int DELAY = 10;
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -83,7 +83,7 @@ public class DinosaurPanel extends JPanel implements ActionListener {
 
 	private int convertWidth(double width) {
 		Insets insets = getInsets();
-		int widthComp = getWidth() - (insets.left + insets.right);
+		int widthComp = getHeight() - (insets.left + insets.right);
 
 		return (int) (width * (widthComp - 1));
 	}
